a83b2de29676966de0a45175a3c375042dc09c26
José Muñoz R <jarmunozr@gmail.com>
2016-03-26 18:11:28 -0300

    se agrega dependencias necesarias

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+import http from 'http'
+import express from 'express'
+
+const app = express()
+const server = http.createServer(app)
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+server.listen(port,() => console.log(`Server runint at http://localhost:${port}/`))
