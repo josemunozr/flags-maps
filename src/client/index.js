0f550bb07ecfb8fa50c10a986b2d397bc715857e
José Muñoz R <jarmunozr@gmail.com>
2016-03-29 00:54:19 -0300

    render geolocalizacion

--- src/client/index.js
+++ src/client/index.js
@@ -1,33 +1,11 @@
 import $ from 'jquery'
 import 'materialize'
-import {initMap, geolocation} from './map'
-
-let lat = ''
-let lng = ''
+import {geolocation} from './map'
 
 $('.button-collapse').sideNav({
   menuWidth: 500,
   edge: 'right'
 })
 
-geolocation((geo) => {
-  let myPosition = {}
-
-  geo.getCurrentPosition((position) => {
-    lat = position.coords.latitude
-    lng = position.coords.longitude
-
-    myPosition = { lat: lat, lng: lng }
-    initMap('map', myPosition)
-  }, (err) => {
-    if (err) {
-      // lat and lng of Santiago, Chile
-      lat = -33.4488897
-      lng = -70.6692655
-
-      myPosition = { lat: lat, lng: lng }
-      initMap('map', myPosition)
-    }
-  }
-  )
-})
+const map = geolocation('map')
+console.log(map)
--- src/client/map/index.js
+++ src/client/map/index.js
@@ -1,15 +1,28 @@
 const google = window.google
 
-export function initMap (id, positions) {
+function _initMap (id, positions) {
   return new google.maps.Map(document.getElementById(id), {
     center: positions,
-    zoom: 12
+    zoom: 13
   })
 }
 
-export function geolocation (callback) {
+export function geolocation (idDom) {
   if (navigator.geolocation) {
-    callback(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude
+      let lng = position.coords.longitude
+
+      return _initMap(idDom, { lat: lat, lng: lng })
+    }, (err) => {
+      if (err) {
+        // lat and lng of Santiago, Chile
+        let lat = -33.4488897
+        let lng = -70.6692655
+
+        return _initMap(idDom, { lat: lat, lng: lng })
+      }
+    })
   }
 }
 
