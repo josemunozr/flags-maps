1db8f98d40279b8cc9a269673cccd3db3b5a1044
José Muñoz R <jarmunozr@gmail.com>
2016-04-13 22:01:50 -0300

    se agregan task a gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,37 @@
 var gulp = require('gulp')
 var jade = require('gulp-jade')
+var babel = require('gulp-babel')
 
 gulp.task('template', function () {
   gulp.src('src/client/**/*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('./public/'))
+    .pipe(jade({
+      locals: 'index.html'
+    }))
+    .pipe(gulp.dest('./public'))
 })
+
+gulp.task('babel', function () {
+  gulp.src('src/server/**/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./dist/src/server'))
+})
+
+gulp.task('copy:img', function () {
+  gulp.src('src/client/asset/img/*.png')
+  .pipe(gulp.dest('./public/img'))
+})
+
+gulp.task('copy:css', function () {
+  gulp.src('src/client/asset/*.css')
+  .pipe(gulp.dest('./public'))
+})
+
+gulp.task('copy', ['copy:img', 'copy:css'])
 
 gulp.task('watch', function () {
   gulp.watch('./src/client/**/*.jade', ['template'])
 })
 
-gulp.task('default', ['watch','template'])
+gulp.task('default', ['watch', 'template', 'babel', 'copy'])
