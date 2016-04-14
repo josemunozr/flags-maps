7ac7d328d84784a285532f6070ca442ca0a77aa3
José Muñoz R <jarmunozr@gmail.com>
2016-04-14 00:11:11 -0300

    se agregan botones sociales

--- src/client/index.js
+++ src/client/index.js
@@ -2,11 +2,6 @@ import $ from 'jquery'
 import 'materialize'
 import {initMap, geolocation, createMarker, autocomplete} from './map'
 
-$('.button-collapse').sideNav({
-  menuWidth: 500,
-  edge: 'right'
-})
-
 initMap('map', {lat: -33.4488897, lng: -70.6692655}, 8)
 
 geolocation((err, position) => {
